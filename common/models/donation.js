f3b0602108c6c0be485e6252ae700757ee80f602
ian seyer <ian.seyer@gmail.com>
2015-10-07 12:40:51 -0500

    Revert "proper model findingi"
    This reverts commit c3d2b085d956201fa15796270b2a35705e288d24.

--- common/models/donation.js
+++ common/models/donation.js
@@ -20,7 +20,7 @@ module.exports = function(Donation) {
   Donation.addCard = function(donor, token, cb){
     console.log(donor)
     console.log(token)
-    app.models.Donor.findById(donor.id)
+    Donor.findById(donor.id)
     .then(function(instance){
       stripe.customers.update(instance.stripeId, {
         source: token
