997ea71155d1c20a01e5815e6b15608e8ffa7d8e
ian seyer <ian.seyer@gmail.com>
2015-10-20 11:23:44 -0500

    facebook login

--- server/server.js
+++ server/server.js
@@ -9,6 +9,39 @@ var _ = require('lodash');
 require('dotenv').load()
 
 app.use(cors());
+
+// Create an instance of PassportConfigurator with the app instance
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+// Enable http session
+app.use(loopback.session({ secret: 'keyboard cat' }));
+boot(app, __dirname);
+// Load the provider configurations
+var config = {};
+try {
+ config = require('./providers.json');
+} catch(err) {
+ console.error('Please configure your passport strategy in `providers.json`.');
+ console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+ process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
+
+// Set up related models
+passportConfigurator.setupModels({
+ userModel: app.models.donor,
+ userIdentityModel: app.models.userIdentity,
+ userCredentialModel: app.models.userCredential
+});
+// Configure passport strategies for third party auth providers
+for(var s in config) {
+ var c = config[s];
+ c.session = c.session !== false;
+ passportConfigurator.configureProvider(s, c);
+}
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
