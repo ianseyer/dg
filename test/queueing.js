8c6a165f52b2da05ab80cd5bbaf829ab302126c0
ian seyer <ian.seyer@gmail.com>
2015-10-27 11:55:09 -0500

    adding in EntryTemplates

--- test/queueing.js
+++ test/queueing.js
@@ -11,13 +11,28 @@ var makeDonation = function(user, amount){
 
 }
 describe("Content Queueing", function(){
-  it("gives donating user content if content exists", function(done){
+  it("gives the donating user content if content exists", function(done){
 
   })
   it("adds cause to user list of pending content if no content exists", function(done){
 
   })
+  it("adds the donor to the causes list of donors", function(done){
+
+  })
   it("finds a user waiting for content when an entry is published", function(done){
+
+  })
+  it('does not give content to a user who is in line BEHIND another user', function(done){
+
+  })
+  it('does not give content from another cause', function(done){
+
+  })
+  it('emails the user to notify them of content', function(done){
+
+  })
+  it('marks the content as no longer being available', function(done){
 
   })
 })
